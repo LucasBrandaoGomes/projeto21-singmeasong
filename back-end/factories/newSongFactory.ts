0f3f83d29587277de8f022a6e33261346e4a4020
lucasbrandao <brandslucas@gmail.com>
2022-09-22 18:50:01 -0300

    get one recommendtion by id

--- back-end/factories/newSongFactory.ts
+++ back-end/factories/newSongFactory.ts
@@ -2,9 +2,10 @@ import { faker } from "@faker-js/faker";
 import { CreateRecommendationData } from "../src/services/recommendationsService";
 
 export async function createNewSong () : Promise <CreateRecommendationData> {
+    const urlStart = "https://www.youtube.com/"
     const song = {
-      name: faker.random.alphaNumeric(),
-      youtubeLink: "https://www.youtube.com/watch?v=HcBFyVnf-_o"
+      name: faker.random.alphaNumeric(4),
+      youtubeLink: urlStart + faker.random.alphaNumeric(15)
     };
 
     return song;
--- back-end/tests/getRecommendations.test.ts
+++ back-end/tests/getRecommendations.test.ts
@@ -1,4 +1,5 @@
 import supertest from "supertest";
+import { createNewSong } from "../factories/newSongFactory";
 import { create15Recommendations } from "../factories/scenarioFactory";
 import app from "../src/app";
 import { prisma }  from "../src/database";
@@ -12,12 +13,32 @@ describe("Test route GET /recommendations", () => {
     it("Returning 10 recommendations",async () => {
         await create15Recommendations()
         const result = await supertest(app).get('/recommendations')
-        const { recommendation } = result.body
+        expect(result.status).toBe(200)
+        expect(result.body.length).toEqual(10)
+        expect(result.body[0].id).toEqual(15)
+        expect(result.body[9].id).toEqual(6)
+        
+    })
+})
+
+describe("Test route GET /recommendations/:id", () => {
+    it("Returning one recomendation, by ID, return status 200",async () => {
+        const newSong = await createNewSong()
+        await supertest(app).post('/recommendations').send(newSong);
+        const createdSong = await prisma.recommendation.findUnique({where: {name:newSong.name}});
+        const { id } = createdSong
+        const result = await supertest(app).get(`/recommendations/${id}`)
 
         expect(result.status).toBe(200)
-        expect(recommendation.length).toEqual(10)
-        expect(recommendation[0].id).toEqual(5)
-        expect(recommendation[9].id).toEqual(15)
+        
+    })
+
+    it("Trying to rReturning one recomendation, by invalid ID, return status 404",async () => {
+        const newSong = await createNewSong()
+        await supertest(app).post('/recommendations').send(newSong);
+        const result = await supertest(app).get(`/recommendations/2`)
+
+        expect(result.status).toBe(404)
         
     })
 })
